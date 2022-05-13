4102e72ff4d8db2d4747fd9be06bcad12790fc3f
GiulsV <valente.giulia93@gmail.com>
2022-05-13 16:40:54 +0200

    Dificoltà 1: genera grid con #100 non casuali

--- script/script.js
+++ script/script.js
@@ -3,3 +3,45 @@
     // con difficoltà 2 => tra 1 e 81
     // con difficoltà 3 => tra 1 e 49
     // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+    
+
+    const grid = document.getElementById('grid');
+
+// Difficoltà 1: tra 1 e 100
+
+    const columns = 10;
+    const rows = 10;
+
+    const totalCells = columns * rows;
+
+
+    for (let i = 1; i <= totalCells; i++){
+
+        const cell = createCell();
+        
+
+        cell.innerText = i;
+        cell.id = i;
+
+        cell.addEventListener('click', function(){
+
+                cell.classList.toggle('bg-blue');
+
+            }
+        );
+
+        grid.appendChild(cell);
+
+        console.log(cell);
+        
+    }
+
+
+    function createCell() {
+
+        const item = document.createElement('div');
+
+        item.classList.add('cell');
+
+        return item;
+    }
